babe8a581602874e397f88aaee156d290a3faf10
송예근 <syg000@cau.ac.kr>
2026-05-15 14:22:03 +0900

    target 기본 선언 수정, 주석 추가, 문자열로 된 숫자 정수로 교체

--- class_UnitManager.py
+++ class_UnitManager.py
@@ -33,10 +33,10 @@ class UnitManager:
         "쿼트" : "qt"
     }
     UNIT2_MAPPING = { #단위 통일 : 수량 변환
-        "컵" : "200",  #1컵 = 200ml 기준
-        "oz" : "30", #1oz = 30ml 기준 (정확히 29.57ml이지만 편의상 30ml로 계산)
-        "pt" : "473", #1pt = 473ml 기준
-        "qt" : "946", #1qt = 946ml 기준
+        "컵" : 200,  #1컵 = 200ml 기준
+        "oz" : 30, #1oz = 30ml 기준 (정확히 29.57ml이지만 편의상 30ml로 계산)
+        "pt" : 473, #1pt = 473ml 기준
+        "qt" : 946, #1qt = 946ml 기준
     }
     UNIT3_MAPPING = { #ml로 통일
         "컵" : "ml",
@@ -44,7 +44,7 @@ class UnitManager:
         "pt" : "ml",
         "qt" : "ml"
     }
-    def unit_conversion(self, target=[]): #타겟
+    def unit_conversion(self, target=[0,0,0,0]): #타겟
 
         '''
         냉장고 재료 추가가 다음과 같은 형식이라 가정하고 코드를 작성하겠다.
@@ -52,9 +52,9 @@ class UnitManager:
         '''
         self.target = target
         unit = str(target[2]).strip().lower() #소문자로 통일, 한글이어도 오류 X
-        if unit in self.UNIT1_MAPPING:
-            target[2] = self.UNIT1_MAPPING[unit]
-        if target[2] in self.UNIT2_MAPPING:
+        if unit in self.UNIT1_MAPPING: #만일 단위가 통일되어야 하는 (수정되어야하는) 경우라면
+            target[2] = self.UNIT1_MAPPING[unit] #그 단위를 통일된 단위로 바꿔준다.
+        if target[2] in self.UNIT2_MAPPING: #만일 단위가 수량 변환이 필요한 단위라면
             target[1] = float(float(target[1]) * float(self.UNIT2_MAPPING[target[2]])) #수량 변환
             target[2] = self.UNIT3_MAPPING[target[2]] #단위 변환
         return target
